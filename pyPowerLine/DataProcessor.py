ea8f56d5351fa2f93fae2063d79b1196dbe05a22
Alex Precosky <precosky@mac.com>
2018-05-23 16:55:10 -0700

    Wrote an abstract DataProcessor class

---
+++ pyPowerLine/DataProcessor.py
@@ -0,0 +1,24 @@
+from abc import ABC, abstractmethod
+
+
+class DataProcessor(ABC):
+    def __init__(self, get_function, put_function):
+        super().__init__()
+
+        self.get_function = get_function
+        self.put_function = put_function
+
+    @abstractmethod
+    def process_line(self, telemetry_line):
+        """Returns a line of processed telemetry and a list of Anomalies"""
+        pass
+
+    def run(self):
+        while True:
+            telemetry_line = self.get_function()
+            if telemetry_line is None:
+                break
+            transfomred_line, anomalies = self.process_line(telemetry_line)
+            self.put_function(transfomred_line)
+            for anomaly in anomalies:
+                self.put_function(anomaly)
---
+++ pyPowerLine/test/test_DataProcessor.py
@@ -0,0 +1,44 @@
+import unittest
+from unittest.mock import MagicMock
+from pyPowerLine import DataProcessor
+
+
+class TestDataProcessor(unittest.TestCase):
+
+    def setUp(self):
+        # allow instantiation of an ABC for testing
+        DataProcessor.DataProcessor.__abstractmethods__ = set()
+
+        self.read_count = 0
+
+    def one_line_read_callback(self):
+
+        if self.read_count == 0:
+            return_line = "1, 2, 3"
+        else:
+            return_line = None
+        self.read_count += 1
+        return return_line
+
+    def null_write_callback(self, write_str):
+        pass
+
+    def test_write_callback_called(self):
+        write_callback = MagicMock()
+
+        processor = DataProcessor.DataProcessor(self.one_line_read_callback, write_callback)
+
+        processor.run()
+
+        write_callback.assert_called_once_with("1, 2, 3")
+
+    def test_process_called(self):
+        mock_process = MagicMock()
+
+        processor = DataProcessor.DataProcessor(self.one_line_read_callback, self.null_write_callback)
+        def null_processor(x): return x
+
+        processor.process_line = mock_process
+        processor.run()
+
+        mock_process.assert_called()
